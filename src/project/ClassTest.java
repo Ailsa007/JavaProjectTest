e6e8c2101b9dd70dfdfed18a966fcc5eb25c38c9
DESKTOP-P4QT5CV\ASUS <ailsatjf001@outlook.com>
2018-07-01 22:52:33 +0800

    Commit_github_1

---
+++ src/project/ClassTest.java
@@ -0,0 +1,24 @@
+package project;
+
+public class ClassTest {
+	
+    //salary是静态的私有变量
+    static double salary;
+    // DEPARTMENT是一个常量
+    public static final String DEPARTMENT = "开发人员";
+	
+	public static void main(String []args) {
+        System.out.println("Hello World"); // 打印 Hello World
+        salary = 10000;
+        System.out.println(DEPARTMENT+"平均工资:"+salary);
+        test();
+            
+        
+    }
+	
+	public static void test()
+	{
+		System.out.println(DEPARTMENT+"pingjungongzi:"+salary);
+	}
+
+}
